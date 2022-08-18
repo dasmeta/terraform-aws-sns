633a4f3c7e0dfae535ead2554fccd9f7f7db2ff1
Karapetyan, Aram (ext.) <aram.karapetyan@proalpha.com>
2022-08-18 13:31:42 +0400

    fix(DMVP-1050): switch to function data

--- modules/subscription/lambda.tf
+++ modules/subscription/lambda.tf
@@ -1,5 +1,4 @@
-data "aws_lambda_alias" "lambda" {
+data "aws_lambda_function" "lambda" {
   count         = var.protocol == "lambda" ? 1 : 0
   function_name = var.endpoint
-  name          = var.endpoint
 }
