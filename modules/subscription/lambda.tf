b4d77a4c4604a0f4147ecde17d5bd21f566ccfab
Aram Karapetyan <aram@dasmeta.com>
2022-09-02 11:24:39 +0400

    DMVP-1050: add missing permission resource to lambda (#3)
    * fix(DMVP-1050): add missing permission for lambda to get trigger created automatically

--- modules/subscription/lambda.tf
+++ modules/subscription/lambda.tf
@@ -2,3 +2,11 @@ data "aws_lambda_function" "lambda" {
   count         = var.protocol == "lambda" ? 1 : 0
   function_name = var.endpoint
 }
+
+resource "aws_lambda_permission" "lambda_with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = var.endpoint
+  principal     = "sns.amazonaws.com"
+  source_arn    = local.topic_arn
+}
--- modules/subscription/subscription.tf
+++ modules/subscription/subscription.tf
@@ -1,4 +1,4 @@
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+resource "aws_sns_topic_subscription" "subscription" {
   topic_arn = local.topic_arn
   protocol  = var.protocol
   endpoint  = local.endpoint
