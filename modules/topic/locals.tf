97023b91aaedc14001e515d477bd5b8a1c0712ff
Aram Karapetyan <aram@dasmeta.com>
2024-11-12 17:27:50 +0400

    fix(DMVP-5761): Fix null policy

--- modules/topic/locals.tf
+++ modules/topic/locals.tf
@@ -2,7 +2,7 @@ locals {
   subscriptions_map  = { for subscription in var.subscriptions : "${subscription.protocol}:${coalesce(subscription.name, sha256(subscription.endpoint))}" => subscription }
   subscriptions_keys = nonsensitive(keys(local.subscriptions_map))
 
-  sns_policy = var.policy == null ? jsonencode{
+  sns_policy = var.policy == null ? jsonencode({
     "Version" : "2008-10-17",
     "Id" : "__default_policy_ID",
     "Statement" : [
